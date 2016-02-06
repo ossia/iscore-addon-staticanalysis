3035b3cbeafcc4eb5480871853e9b4dc23097d68
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-02-06 23:02:42 +0100

    update

--- iscore_plugin_staticanalysis.cpp
+++ iscore_plugin_staticanalysis.cpp
@@ -15,3 +15,13 @@ iscore::GUIApplicationContextPlugin* iscore_addon_staticanalysis::make_applicati
 {
     return new stal::ApplicationPlugin{app};
 }
+
+iscore::Version iscore_addon_staticanalysis::version() const
+{
+    return iscore::Version{1};
+}
+
+UuidKey<iscore::Plugin> iscore_addon_staticanalysis::key() const
+{
+    return "e1ef22f4-5fa3-4992-9f88-0e1ec5b5bb7f";
+}
--- iscore_plugin_staticanalysis.hpp
+++ iscore_plugin_staticanalysis.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <iscore/plugins/qt_interfaces/PluginRequirements_QtInterface.hpp>
 #include <iscore/plugins/qt_interfaces/GUIApplicationContextPlugin_QtInterface.hpp>
 #include <QObject>
 
@@ -13,11 +14,13 @@ namespace TA
 }
 class iscore_addon_staticanalysis final:
         public QObject,
+        public iscore::Plugin_QtInterface,
         public iscore::GUIApplicationContextPlugin_QtInterface
 {
         Q_OBJECT
         Q_PLUGIN_METADATA(IID GUIApplicationContextPlugin_QtInterface_iid)
         Q_INTERFACES(
+                iscore::Plugin_QtInterface
                 iscore::GUIApplicationContextPlugin_QtInterface
                 )
 
@@ -27,5 +30,8 @@ class iscore_addon_staticanalysis final:
 
         iscore::GUIApplicationContextPlugin* make_applicationPlugin(
                 const iscore::ApplicationContext& app) override;
+
+        iscore::Version version() const override;
+        UuidKey<iscore::Plugin> key() const override;
 
 };
