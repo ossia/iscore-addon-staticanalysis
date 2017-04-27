5ec5f47d03122d9453989b17c0d63eb90900e86f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-04-27 13:05:35 +0200

    refactor

--- iscore_plugin_staticanalysis.hpp
+++ iscore_plugin_staticanalysis.hpp
@@ -15,13 +15,13 @@ namespace TA
 class iscore_addon_staticanalysis final:
         public QObject,
         public iscore::Plugin_QtInterface,
-        public iscore::GUIApplicationPlugin_QtInterface
+        public iscore::ApplicationPlugin_QtInterface
 {
         Q_OBJECT
-        Q_PLUGIN_METADATA(IID GUIApplicationPlugin_QtInterface_iid)
+        Q_PLUGIN_METADATA(IID ApplicationPlugin_QtInterface_iid)
         Q_INTERFACES(
                 iscore::Plugin_QtInterface
-                iscore::GUIApplicationPlugin_QtInterface
+                iscore::ApplicationPlugin_QtInterface
                 )
 
   ISCORE_PLUGIN_METADATA(1, "e1ef22f4-5fa3-4992-9f88-0e1ec5b5bb7f")
