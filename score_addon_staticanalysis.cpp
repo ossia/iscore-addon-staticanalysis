a467e5cbb1672454790c2e9c342f9d1daab2a030
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-08-09 10:30:50 +0200

    update with score

---
+++ score_addon_staticanalysis.cpp
@@ -0,0 +1,18 @@
+#include <StaticAnalysis/ScenarioVisitor.hpp>
+#include "score_addon_staticanalysis.hpp"
+
+score_addon_staticanalysis::score_addon_staticanalysis()
+{
+#if defined(SCORE_STATIC_PLUGINS)
+  Q_INIT_RESOURCE(TAResources);
+#endif
+}
+
+score::GUIApplicationPlugin* score_addon_staticanalysis::make_guiApplicationPlugin(
+        const score::GUIApplicationContext& app)
+{
+    return new stal::ApplicationPlugin{app};
+}
+
+#include <score/plugins/PluginInstances.hpp>
+SCORE_EXPORT_PLUGIN(score_addon_staticanalysis)
